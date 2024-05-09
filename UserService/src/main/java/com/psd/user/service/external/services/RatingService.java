6fca5b5e5904ce75822e59092d0e8266383214a9
prash97 <prash9727.ps@gmail.com>
2024-05-09 20:49:23 +0530

    fiegnclient

---
+++ UserService/src/main/java/com/psd/user/service/external/services/RatingService.java
@@ -0,0 +1,26 @@
+package com.psd.user.service.external.services;
+
+import com.psd.user.service.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "RATINGSERVICE")
+public interface RatingService {
+
+
+    //get
+    @GetMapping
+    Rating getRatings(String ratingId);
+
+    //post
+    @PostMapping
+    Rating create(Rating rating);
+
+    //update
+    @PutMapping("/ratings/{ratingId}")
+    Rating updateRating(@PathVariable String ratingId, Rating rating);
+
+    //delete
+    @DeleteMapping("/ratings/{ratingId}")
+    void deleteRating(@PathVariable String ratingId);
+}
