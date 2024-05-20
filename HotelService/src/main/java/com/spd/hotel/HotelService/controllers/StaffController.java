03725509ff3da94ca67b340ab1b6dfd252923263
prash97 <prash9727.ps@gmail.com>
2024-05-20 23:21:57 +0530

    added API Gateway service

---
+++ HotelService/src/main/java/com/spd/hotel/HotelService/controllers/StaffController.java
@@ -0,0 +1,22 @@
+package com.spd.hotel.HotelService.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaff(){
+        List<String> staff = Arrays.asList("Prashant", "Sampada", "Varsha", "Pankaj");
+        return new ResponseEntity<>(staff, HttpStatus.OK);
+    }
+}
